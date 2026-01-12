91c82d67e84d487715102f2a65e68e2058aaeed1
Shikhar277 <6shikharsingh@gmail.com>
2026-01-12 19:50:16 +0530

    Time: 2 ms (99.17%), Space: 43.9 MB (77.5%) - LeetHub

---
+++ 3305-count-of-substrings-containing-every-vowel-and-k-consonants-i/3305-count-of-substrings-containing-every-vowel-and-k-consonants-i.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int countOfSubstrings(String word, int k) {
+        if(k==0)
+        return substrings(word,k);
+        
+        return substrings(word,k)-substrings(word,k-1);// atmost k -atmost k-1
+    }
+    public int substrings(String word,int k)// atmost k consonants + atleast all vowels covered
+    {
+       int a=0,e=0,i=0,o=0,u=0;
+       int La=-1,Le=-1,Li=-1,Lo=-1,Lu=-1; // last seen indices
+       int count_k=0;
+       int l=0,r=0;
+       int count_substrings=0;
+       int minLastScene=0;// Minimum of last index of any vowel in the window
+       while(r<word.length())
+       {
+         char ch=word.charAt(r);
+         if(ch=='a'){ a++; La = r; }
+         else if(ch=='e'){ e++; Le = r; }
+         else if(ch=='i'){ i++; Li = r; }
+         else if(ch=='o'){ o++; Lo = r; }
+         else if(ch=='u'){ u++; Lu = r; }
+         else count_k++;
+
+         while(count_k>k)// condition only on k not vowels to shrink the window(IMP)
+         {
+            char c=word.charAt(l);
+            if(c=='a') a--;
+            else if(c=='e') e--;
+            else if(c=='i') i--;
+            else if(c=='o') o--;
+            else if(c=='u') u--;
+            else count_k--; 
+            l++;       
+         }
+         if(a>=1 && e>=1 && i>=1 && o>=1 && u>=1){
+           int minLastSeen = Math.min(La, Math.min(Le, Math.min(Li, Math.min(Lo, Lu))));
+           count_substrings += (minLastSeen - l + 1);
+         }
+           r++;
+        }
+        return count_substrings;
+    }
+}
