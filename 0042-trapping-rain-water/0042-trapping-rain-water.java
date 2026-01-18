ecce9443e8469b9777a3e35ea0980962b174cab0
Shikhar277 <6shikharsingh@gmail.com>
2026-01-19 01:15:30 +0530

    Time: 0 ms (100%), Space: 47.2 MB (97.35%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,25 +1,28 @@
 class Solution {
     public int trap(int[] height) {
-      // solve using prefix max and suffix max
+      // solve using two pointer left and right
       int n=height.length;
-      int pre_max[]=new int[n];
-      int suff_max[]=new int[n];
-      pre_max[0]=height[0];
-      suff_max[n-1]=height[n-1];
-
-      for(int i=1;i<n;i++)
-         pre_max[i]=Math.max(pre_max[i-1],height[i]);
-
-      for(int i=n-2;i>=0;i--)
-         suff_max[i]=Math.max(suff_max[i+1],height[i]);  
-
-      int total_trap=0; // total trapped water
-      for(int i=1;i<n-1;i++)
+      int l=0,r=n-1;
+      int lmax=height[l],rmax=height[r];
+      int total_trap=0;
+      while(l<r)
       {
-        int left_max=pre_max[i];
-        int right_max=suff_max[i];
-        if(height[i]<left_max && height[i]<right_max)
-           total_trap+=Math.min(left_max,right_max)-height[i];
+         if(height[l]<=height[r])
+         {
+           if(lmax>height[l])
+              total_trap+=lmax-height[l];
+           else
+              lmax=height[l];
+           l++;
+         }
+         else
+         {
+            if(rmax>height[r])
+              total_trap+=rmax-height[r];
+            else
+              rmax=height[r];
+           r--;
+         }
       }
       return total_trap;
     }
