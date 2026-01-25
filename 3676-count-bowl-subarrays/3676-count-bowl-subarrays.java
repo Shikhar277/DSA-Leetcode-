234ae51e2a7e7c38baeb08a3b21e5980cac125db
Shikhar277 <6shikharsingh@gmail.com>
2026-01-25 16:11:37 +0530

    Time: 161 ms (8.13%), Space: 127.5 MB (30.63%) - LeetHub

---
+++ 3676-count-bowl-subarrays/3676-count-bowl-subarrays.java
@@ -0,0 +1,53 @@
+class Solution {
+    public long bowlSubarrays(int[] nums) {
+       // Intution 
+       // For every maximum element in the array you have to find its boundary means its next greater and previous greater element as  
+       // given min(nums[l], nums[r]) > max(nums[l + 1], ..., nums[r - 1])
+       // count= sum of all sub arrays in which nums[0] is maximum , nums[1] so on... uptill last.
+
+
+       // We could have used the sliding window but condition is not monotonous rather the condition is such it demands some stacks nge and pge 
+       // algorithm and most importantly we are able to do this here since we are sure for every element there will be 1 sub array or there     will not be any sub array
+       int pge[]=pge(nums);
+       int nge[]=nge(nums);
+       long count=0;
+       for(int i=0;i<nums.length;i++)
+       {
+         int l=pge[i];
+         int r=nge[i];
+         if(l!=-1 && r!=nums.length && Math.min(nums[l], nums[r]) > nums[i])
+            count++;
+       }
+    return count;
+    }
+    public int[] pge(int[] arr) // prev greater element
+    {
+       Stack<Integer> st=new Stack<>(); // store indexes
+       int n=arr.length;
+       int pge[]=new int[n];
+       for(int i=0;i<n;i++)
+       {
+         while(!st.isEmpty() && arr[st.peek()]<=arr[i])
+         st.pop();
+         
+         pge[i]=st.isEmpty()?-1:st.peek();
+         st.push(i);
+       }
+       return pge;
+    }
+     public int[] nge(int[] arr) // next greater element
+    {
+       Stack<Integer> st=new Stack<>(); // store indexes
+       int n=arr.length;
+       int nge[]=new int[n];
+       for(int i=n-1;i>=0;i--)
+       {
+         while(!st.isEmpty() && arr[st.peek()]<=arr[i])
+         st.pop();
+         
+         nge[i]=st.isEmpty()?n:st.peek();
+         st.push(i);
+       }
+       return nge;
+    }
+}
